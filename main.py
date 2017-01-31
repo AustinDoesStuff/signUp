e356010ba619d5a52f939b3a29dee2b1235f7d9c
Austin Tanner <austin.tanner04@gmail.com>
2017-01-30 20:19:52 -0600

    Added validation, user body and welcomeHandler

--- main.py
+++ main.py
@@ -15,11 +15,77 @@
 # limitations under the License.
 #
 import webapp2
+import re
+import cgi
+
+def BuildPage(userError, passError, emailError):
+    header = """
+    <html>
+        <head>
+        </head>
+        <body>
+    """
+
+    heading = "<h1>Sign up plz</h1><br><br>"
+
+    mainBody = """
+        <form action = '/' method='post'>
+            <label>Username:<input type='text' name='user'></input></label><p>{user}</p>
+            <label>Password:<input type='password' name='pass'></input></label><p>{password}</p>
+            <label>Email:<input type='text' name='email'></input></label><p>{email}</p>
+            <br>
+            <input type='submit'>
+        </form>
+    """.format(user = userError, password = passError, email = emailError)
+    footer = """
+        </body>
+    </html>
+    """
+    page = header + heading + mainBody + footer
+    return page
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        content = BuildPage("","","")
+        self.response.write(content)
+
+    def post(self):
+
+        def valid(regex, testCase):
+            return regex.match(testCase)
+
+        username = cgi.escape(self.request.get('user'))
+        password = cgi.escape(self.request.get('pass'))
+        email = cgi.escape(self.request.get('email'))
+
+        userRE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        passRE = re.compile(r"^.{3,20}$")
+        emailRE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+        userMessage = ""
+        passMessage = ""
+        emailMessage = ""
+
+        if valid(userRE, username) == None:
+            userMessage = "Hey, that's not a valid username, idiot"
+        if valid(passRE, password) == None:
+            passMessage = "Your password sucks, try again"
+        if valid(emailRE, email) == None:
+            emailMessage = "You broke the email, dummy"
+
+        if len(userMessage) > 1 or len(passMessage) > 1 or len(emailMessage) > 1:
+            self.response.write(BuildPage(userMessage,passMessage,emailMessage))
+        else:
+            self.redirect('/welcome')
+
+
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Thanks for logging in, {username}!").format(MainHandler.post("username"))
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', WelcomeHandler)
 ], debug=True)
